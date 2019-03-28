64f7bf3747b69a57019840acdc7164c9e21d9d8e
qinweijunlin <qinweijunlin@qq.com>
2019-03-28 13:37:34 +0800

    发布

--- gitbash/gitbash.cmd
+++ gitbash/gitbash.cmd
@@ -3,7 +3,7 @@ title %~nx0
 
 REM 变量
 set RootDir="%~dp0"
-set DestDir="C:\Program Files\Git"
+set GitBash="C:\Program Files\Git\git-bash.exe"
 set GitSrc=Git-2.21.0-64-bit.exe
 set GitVer=v2.21.0.windows.1
 set GitUrl=https://npm.taobao.org/mirrors/git-for-windows/%GitVer%/%GitSrc%
@@ -12,18 +12,8 @@ REM 切换当前目录
 cd /d %RootDir%
 
 REM 检查 GitBash
-if exist %DestDir%\git-bash.exe (
-  call :EchoError GitBash 已安装！
-)
-
-setlocal enabledelayedexpansion
-
-if exist D:\ (
-  set DestDir="D:\yzaj\Git"
-  
-  if exist !DestDir!\git-bash.exe (
-    call :EchoError GitBash 已安装！
-  )
+if exist %GitBash% (
+  call :EchoMessage GitBash 已安装！
 )
 
 REM 下载 GitBash
@@ -32,23 +22,21 @@ if not exist %GitSrc% (
 )
 
 if not exist %GitSrc% (
-  call :EchoError GitBash 下载失败！
+  call :EchoMessage Error - GitBash 下载失败！
 )
 
 REM 安装 GitBash
 echo,正在安装 GitBash ...
-start /wait "" %GitSrc% /sp- /silent /norestart /dir=!DestDir!
+start /wait "" %GitSrc% /sp- /silent /norestart
 
-if not exist !DestDir!\git-bash.exe (
-  call :EchoError GitBash 安装失败！
+if not exist %GitBash% (
+  call :EchoMessage Error - GitBash 安装失败！
 )
 
-endlocal
-
-REM 标签
 exit
-:EchoError
-echo,Error: %*
+REM 标签
+:EchoMessage
+echo,%*
 pause>nul
 exit
 goto:eof
